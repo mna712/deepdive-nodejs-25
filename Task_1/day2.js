8894c7e4f29e86bdb3c605da393aec7e16d14758
Menna Elgharabawii <162707844+mna712@users.noreply.github.com>
2025-08-07 19:28:07 +0300

    Update day2.js

--- Task_1/day2.js
+++ Task_1/day2.js
@@ -7,4 +7,35 @@ function getGrade(score) {
                     : score>5&&score<=10? grade="E"
                         :grade="F";
     return grade;
+
+}
+
+function getLetter(s) {
+    let letter;
+    switch (s[0]) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            letter = 'A';
+            break;
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'f':
+        case 'g':
+            letter = 'B';
+            break;
+        case 'h':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'm':
+            letter = 'C';
+            break;
+        default:
+            letter = 'D';
+    }
+    return letter;
 }
